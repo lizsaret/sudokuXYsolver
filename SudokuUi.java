be18fe83ee63fd219784f01f6d8c54434d6143a1
Anjelle <aadispo@up.edu.ph>
2018-05-10 03:32:41 +0800

    Changed UI Layout

--- SudokuUi.java
+++ SudokuUi.java
@@ -12,14 +12,34 @@ public class SudokuUi {
 	private Container frame = mainFrame.getContentPane();
 
 	private JPanel titlePanel = new JPanel();
-	private JPanel contentsPanel = new JPanel(new BorderLayout());
-	private JPanel givenPanel = new JPanel();
-	private JPanel optionsPanel = new JPanel();
-	private JPanel answersPanel = new JPanel();
+	private JPanel sudokuPanel1 = new JPanel();
+	private JPanel sudokuPanel2 = new JPanel(new BorderLayout());
+	private JPanel optionsPanel1 = new JPanel();
+	private JPanel optionsPanel2 = new JPanel();
+	private JPanel optionsPanel3 = new JPanel();
+	private JPanel browseButtonPanel = new JPanel();
+	private JPanel solutionRadioPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	private JPanel solutionLabelPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	private JPanel solutionButtonPanel = new JPanel();
+	private JPanel restartButtonPanel = new JPanel();
+
+	// private JPanel givenPanel = new JPanel();
+	// private JPanel answersPanel = new JPanel();
 
 	private JButton browseButton = new JButton("Open Input File");
+	private JButton restartButton = new JButton("Restart Grid");
+	private JButton solutionButton = new JButton("Show Solution");
+
+	private JRadioButton solutionRegular = new JRadioButton("Sudoku Regular");
+	private JRadioButton solutionX = new JRadioButton("Sudoku X");
+	private JRadioButton solutionY = new JRadioButton("Sudoku Y");
+	private JRadioButton solutionXY = new JRadioButton("Sudoku XY");
 
 	private JLabel currentPuzzleLabel = new JLabel("");
+	private JLabel solutionNoRegular = new JLabel("Sudoku Solutions: ");
+	private JLabel solutionNoX = new JLabel("Sudoku X Solutions: ");
+	private JLabel solutionNoY = new JLabel("Sudoku Y Solutions: ");
+	private JLabel solutionNoXY = new JLabel("Sudoku XY Solutions: ");
 	private JTable currGivenTable;
 
 	private int currentPuzzlePointer = 0;
@@ -34,13 +54,65 @@ public class SudokuUi {
 	private void init() {
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setResizable(false);
-		mainFrame.setPreferredSize(new Dimension(1040, 600));
+		mainFrame.setPreferredSize(new Dimension(1000, 600));
 
 		frame.setLayout(new BorderLayout());
 		frame.add(titlePanel, BorderLayout.NORTH);
-		frame.add(contentsPanel, BorderLayout.CENTER);
-		
-		titlePanel.add(browseButton);
+		frame.add(optionsPanel1, BorderLayout.WEST);
+		frame.add(sudokuPanel1, BorderLayout.EAST);
+
+		titlePanel.setBackground(Color.BLACK);
+		titlePanel.setPreferredSize(new Dimension(1000, 100));
+
+		optionsPanel1.setLayout(new BorderLayout());
+		optionsPanel1.setBackground(Color.GRAY);
+		optionsPanel1.setPreferredSize(new Dimension(190, 500));
+		optionsPanel1.add(optionsPanel2, BorderLayout.NORTH);
+		optionsPanel1.add(optionsPanel3, BorderLayout.SOUTH);
+
+		optionsPanel2.setLayout(new BorderLayout());
+		optionsPanel2.setOpaque(false);
+		optionsPanel2.setPreferredSize(new Dimension(190, 180));
+		optionsPanel2.add(browseButtonPanel, BorderLayout.NORTH);
+		optionsPanel2.add(solutionLabelPanel);
+		optionsPanel2.add(restartButtonPanel, BorderLayout.SOUTH);
+
+		optionsPanel3.setLayout(new BorderLayout());
+		optionsPanel3.setOpaque(false);
+		optionsPanel3.setPreferredSize(new Dimension(190, 180));
+		optionsPanel3.add(solutionRadioPanel);
+		optionsPanel3.add(solutionButtonPanel, BorderLayout.SOUTH);
+
+		browseButtonPanel.add(browseButton);
+		browseButtonPanel.setOpaque(false);		
+
+		solutionLabelPanel.add(solutionNoRegular);
+		solutionLabelPanel.add(solutionNoX);
+		solutionLabelPanel.add(solutionNoY);
+		solutionLabelPanel.add(solutionNoXY);
+		solutionLabelPanel.setOpaque(false);
+
+		restartButtonPanel.add(restartButton);
+		restartButtonPanel.setOpaque(false);
+
+		solutionRadioPanel.add(solutionRegular);
+		solutionRadioPanel.add(solutionX);
+		solutionRadioPanel.add(solutionY);
+		solutionRadioPanel.add(solutionXY);
+		solutionRadioPanel.setOpaque(false);
+
+		solutionRegular.setOpaque(false);
+		solutionRegular.setForeground(Color.WHITE);		
+		solutionX.setOpaque(false);
+		solutionX.setForeground(Color.WHITE);
+		solutionY.setOpaque(false);
+		solutionY.setForeground(Color.WHITE);
+		solutionXY.setOpaque(false);
+		solutionXY.setForeground(Color.WHITE);
+
+		solutionButtonPanel.add(solutionButton);
+		solutionButtonPanel.setOpaque(false);
+
 		
 		// browse button action listener
 		browseButton.addActionListener(new ActionListener() {
@@ -57,11 +129,11 @@ public class SudokuUi {
 			}
 		});
 
-		contentsPanel.add(givenPanel, BorderLayout.WEST);
-		contentsPanel.add(optionsPanel, BorderLayout.CENTER);
-		contentsPanel.add(answersPanel, BorderLayout.EAST);
-
-		givenPanel.add(currentPuzzleLabel);
+		sudokuPanel1.setPreferredSize(new Dimension(810, 500));
+		sudokuPanel1.add(currentPuzzleLabel);
+		// contentsPanel.add(givenPanel, BorderLayout.WEST);
+		// contentsPanel.add(optionsPanel, BorderLayout.CENTER);
+		// contentsPanel.add(answersPanel, BorderLayout.EAST);
 		// contentsPanel.setBorder(BorderFactory.createLineBorder(Color.CYAN,5));
 		// givenPanel.setBorder(BorderFactory.createLineBorder(Color.RED,5));
 		// answersPanel.setBorder(BorderFactory.createLineBorder(Color.GREEN,5));
@@ -83,7 +155,8 @@ public class SudokuUi {
 			}
 		}
 
-		givenPanel.add(currGivenTable);
+
+		sudokuPanel2.add(currGivenTable);
 	}
 
 	public void displayCurrentSolution(Puzzle currentPuzzle, int index) {
